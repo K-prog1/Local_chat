a9064bfc8af53d1e36ccde45223565db1b9db776
Kupaki <gercovdaniil21@gmail.com>
2026-04-27 22:36:21 +1000

    Trying to make local chat

--- just_learning/lesson1/main.cpp
+++ just_learning/lesson1/main.cpp
@@ -2,3 +2,71 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <optional>
+
+
+int main(){
+    sf::TcpSocket socket;
+    std::optional<sf::IpAddress> ipOpt = sf::IpAddress::getLocalAddress();
+
+    if(!ipOpt){
+        std::cout << "Error getting local address!\n";
+        return 1;
+    }
+    sf::IpAddress ip = *ipOpt;
+
+    char type;
+    std::cout << "Enter type connecting; [c] - client, [s] - server";
+    std::cin >> type;
+
+    if (type == 's')
+    {
+        sf::TcpListener listener;
+        listener.listen(2000);
+
+        if(listener.accept(socket) != sf::Socket::Status::Done)
+        {
+            std::cout << "Error!\n";
+        }
+    }
+    else if(type == 'c')
+    {
+        if (socket.connect(ip, 2000) != sf::Socket::Status::Done)
+            {
+                std::cout << "Error! \n";
+            }
+    }
+
+    std::string name;
+    std::cout << "Enter your name: \n";
+    std::cin >> name;
+
+    socket.setBlocking(false);
+
+    std::string message = " ";
+    sf::Packet packet;
+
+    while(true)
+    {
+        if(GetAsyncKeyState(VK_ESCAPE)){
+            std::cin >>message;
+        }
+        if (message != "")
+        {
+            packet.clear();
+            packet << name << message;
+
+            socket.send(packet);
+            message = "";
+        }
+
+        if(socket.receive(packet) == sf::Socket::Status::Done){
+            std::string nameRec;
+            std::string messageRec;
+            packet >> nameRec >> messageRec;
+            std::cout <<nameRec << ": " << messageRec <<"";
+        }
+    }
+    
+    
+}
